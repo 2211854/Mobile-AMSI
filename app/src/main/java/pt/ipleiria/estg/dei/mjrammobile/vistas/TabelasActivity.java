7a029f8806d3825c1d00a4aad3666b089ae6c8b4
ricardosantos2003 <2211925@my.ipleiria.pt>
2022-11-22 15:42:38 +0000

    Login acabado
    Login acabado e comeco da lista de tarefas

---
+++ app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/vistas/TabelasActivity.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.estg.dei.mjrammobile.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipleiria.estg.dei.mjrammobile.R;
+
+public class TabelasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabelas);
+    }
+}
