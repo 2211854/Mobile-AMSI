a8f025d606b4915dee3b093e2d7d7ae5dd2080f6
ricardosantos2003 <2211925@my.ipleiria.pt>
2022-11-23 17:12:35 +0000

    table detalhes terminada
    table detalhes terminada

---
+++ app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/vistas/DetalhesActivity.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.estg.dei.mjrammobile.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipleiria.estg.dei.mjrammobile.R;
+
+public class DetalhesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}
