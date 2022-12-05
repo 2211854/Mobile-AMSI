5258070e89832b3d5ff072c63ce5ef56989e111b
Ricardo santos <2211925@my.ipleiria.pt>
2022-12-05 17:55:40 +0000

    menu quase completo

--- app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/MainActivity.java
@@ -1,30 +1,57 @@
 package pt.ipleiria.estg.dei.mjrammobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import pt.ipleiria.estg.dei.mjrammobile.databinding.ActivityMainBinding;
+import pt.ipleiria.estg.dei.mjrammobile.fragments.HomeFragment;
+import pt.ipleiria.estg.dei.mjrammobile.fragments.PerfilFragment;
+import pt.ipleiria.estg.dei.mjrammobile.fragments.TabelasFragment;
 import pt.ipleiria.estg.dei.mjrammobile.vistas.DetalhesActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.perfil:
+                    replaceFragment(new PerfilFragment());
+                    break;
+                case R.id.avioes:
+                    replaceFragment(new TabelasFragment());
+                    break;
+            }
+
+
+            return true;
+        });
     }
 
-    public void onClickAviao(View view) {
+    private void replaceFragment(Fragment fragment){
 
-        Intent intent = new Intent(this, DetalhesActivity.class);
-        startActivity(intent);
-    }
-    public void onClickPerfil(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.Fl_menu, fragment);
+        fragmentTransaction.commit();
 
-        Intent intent = new Intent(this, DetalhesActivity.class);
-        startActivity(intent);
     }
+
 }
