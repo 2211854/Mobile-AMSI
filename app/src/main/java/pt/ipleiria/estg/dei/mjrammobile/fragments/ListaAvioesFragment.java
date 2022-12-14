2bfe13f851562c43c704a68b4255e4027fc1d7df
Ricardo santos <2211925@my.ipleiria.pt>
2022-12-14 13:56:58 +0000

    seearch

--- app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/fragments/ListaAvioesFragment.java
+++ app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/fragments/ListaAvioesFragment.java
@@ -5,20 +5,21 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import pt.ipleiria.estg.dei.mjrammobile.R;
 import pt.ipleiria.estg.dei.mjrammobile.adaptadores.ListaAviaoAdaptador;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListaAvioesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ListaAvioesFragment extends Fragment {
 
     String Empresas[] = {"TAP","TAP","TAP"};
@@ -27,37 +28,13 @@ public class ListaAvioesFragment extends Fragment {
 
     private View v;
     ListView listview;
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListaAvioesFragment() {
-        // Required empty public constructor
-    }
-
-    public static ListaAvioesFragment newInstance(String param1, String param2) {
-        ListaAvioesFragment fragment = new ListaAvioesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ArrayAdapter adapter;
+    private TextInputEditText textFilter;
+    protected ListaAviaoAdaptador listaAviaoAdaptador;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -66,8 +43,10 @@ public class ListaAvioesFragment extends Fragment {
 
         v = inflater.inflate(R.layout.fragment_lista_avioes, container, false);
 
+        textFilter = v.findViewById(R.id.searchFilter);
+
         listview = v.findViewById(R.id.lvAviao);
-        ListaAviaoAdaptador listaAviaoAdaptador = new ListaAviaoAdaptador(getContext(), Empresas, Estados,  NomeAviaos);
+        listaAviaoAdaptador = new ListaAviaoAdaptador(getContext(), Empresas, Estados,  NomeAviaos);
         listview.setAdapter(listaAviaoAdaptador);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -75,6 +54,24 @@ public class ListaAvioesFragment extends Fragment {
                 FragmentTransaction fr = getFragmentManager().beginTransaction();
                 fr.replace(R.id.Fl_menu, new DetalhesAviaoFragment());
                 fr.commit();
+            }
+        });
+
+        adapter = new ArrayAdapter(getContext(), R.layout.fragment_lista_avioes);
+
+        textFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2){
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                (ListaAvioesFragment.this).adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
--- app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/fragments/ListaTarefasFragment.java
+++ app/src/main/java/pt/ipleiria/estg/dei/mjrammobile/fragments/ListaTarefasFragment.java
@@ -2,13 +2,21 @@ package pt.ipleiria.estg.dei.mjrammobile.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,39 +25,15 @@ import pt.ipleiria.estg.dei.mjrammobile.R;
 
 public class ListaTarefasFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListaTarefasFragment() {
-        // Required empty public constructor
-    }
-    public static ListaTarefasFragment newInstance(String param1, String param2) {
-        ListaTarefasFragment fragment = new ListaTarefasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_lista_tarefas, container, false);
 
@@ -64,6 +48,9 @@ public class ListaTarefasFragment extends Fragment {
             }
         });
 
+
+
         return v;
     }
+
 }
